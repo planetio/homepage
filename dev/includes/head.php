4dc50be4aa6c05b63a83475462f3d8ecb4cadefe
Alex Grande <grandecomplex@gmail.com>
2013-08-23 18:06:13 -0700

    updated keywords

--- dev/includes/head.php
+++ dev/includes/head.php
@@ -4,16 +4,16 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta name="viewport" content="initial-scale=1, user-scalable=no, maximum-scale=1" />
-	<title>Planet | <?= $pageTitle ?> | Web Development</title>
+	<title>Planet | <?= $pageTitle ?> | Interactive Agency</title>
   <meta name="google-site-verification" content="LkZ8-Xel0EqIYISuYoydd3YcaHM6F8jDyJ329wrZeWU" />  <link rel="stylesheet" href="/assets/stylesheets/all.css" type="text/css" media="screen" title="no title" charset="utf-8">
   
 	<link rel="shortcut icon" href="/assets/images/icon.png" />
   <link rel="image_src" href="/assets/images/icon.png" />
   
-  <meta name="description" content="A digital agency transforming great ideas into responsive web experiences." />
-	<meta name="keywords" content="responsive layouts, responsive design, ruby on rails, ror, alex grande, san francisco, mission, web development, JavaScript development sf, phonegap development" />
+  <meta name="description" content="An interactive agency transforming great ideas into responsive web experiences." />
+	<meta name="keywords" content="Planet interactive agency" />
   
-  <meta property="og:title" content="Planet | Web Development Agency" />
+  <meta property="og:title" content="Planet | Interactive Agency" />
   <meta property="og:type" content="website" />
   <meta property="og:site_name" content="Planet.io" />
   <meta property="og:description" content="A digital agency transforming great ideas into responsive web experiences." />
